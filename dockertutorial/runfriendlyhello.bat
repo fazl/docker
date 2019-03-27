56fb32f00ea67ef21dd7de7b0454875f7cf02e40
fazl <5173704+fazl@users.noreply.github.com>
2019-03-27 09:04:45 +0300

    Start following tutorial (docs.docker.com/get-started)

---
+++ dockertutorial/runfriendlyhello.bat
@@ -0,0 +1,10 @@
+#@echo off
+#rem From https://docs.docker.com/get-started/part2/
+#rem 
+echo Run this inside Docker Toolbox on Win7
+echo "docker run -p 4000:80 friendlyhello"
+echo ""
+echo Then get your docker.machine.ip.address via command:
+echo "docker-machine ip"
+echo ""
+echo Then browse e.g http://192.168.99.100:4000/.
